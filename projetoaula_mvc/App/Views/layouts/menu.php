61e122dacec3dcf9680cda38d4e550172c17fa4f
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2023-05-09 21:40:08 -0300

    Update menu.php
    Criando o Menu (Barra de Navegação).
    
    Home; Cadastro de Produtos; Editar Produtos; Excluir Produtos; Listar Produtos;

--- projetoaula_mvc/App/Views/layouts/menu.php
+++ projetoaula_mvc/App/Views/layouts/menu.php
@@ -1,1 +1,27 @@
-
+<nav class="navbar navbar-expand-lg" style="background-color: #9696ff;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">My Produto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastro de Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Editar Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Excluir Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Listar Produtos</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
