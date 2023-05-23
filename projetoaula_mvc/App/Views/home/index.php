b1aa0aa46826dc320c70742633725557a1172404
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2023-05-23 19:59:54 -0300

    Atualização index.php
    Adicionando exemplo de página home com imagens e cards.
    Duas imagens da internet e uma local da pasta: 
    projetoaula_mvc/App/Views/imagens/

--- projetoaula_mvc/App/Views/home/index.php
+++ projetoaula_mvc/App/Views/home/index.php
@@ -1,1 +1,42 @@
-
+<br>
+<div class="container">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card h-100">
+        <img src="https://herbissimo.com.br/wp-content/uploads/2023/03/SHAMPOO-MINT-1000X1000-copiar.png" class="card-img-top" alt="Shampoo">
+        <div class="card-body">
+          <h5 class="card-title">Shampoo</h5>
+          <p class="card-text">O Shampoo do momento!</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary">Atualizado em: 16/05/2023</small>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="https://i0.wp.com/www.realorigami.com.br/wp-content/uploads/2016/03/exemplo.jpg" class="card-img-top" alt="Bicicleta">
+        <div class="card-body">
+          <h5 class="card-title">Bicicleta</h5>
+          <p class="card-text">A melhor bicicleta do mercado!</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary">Atualizado em: 16/05/2023</small>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="<?php echo PATH_IMAGENS;?>computador.jpg" class="card-img-top" alt="Computador">
+        <div class="card-body">
+          <h5 class="card-title">Computador</h5>
+          <p class="card-text">O melhor computador para o seu dia a dia!</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary">Atualizado em: 16/05/2023</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
