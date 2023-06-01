1ad4c325b5064e5a3579eb14eeb6af0c0db24f9c
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2023-06-01 19:31:47 -0300

    Adicionando pasta produto
    Adicionando pasta produto e os arquivos de cadastro, edição e exclusçao.

---
+++ projetoaula_mvc/App/Views/produto/cadastro.php
@@ -0,0 +1,25 @@
+<div class="container">
+<br>
+<h3>CADASTRO DE PRODUTO</h3>
+<br>
+<form class="row g-3">
+
+  <div class="col-md-1">
+    <label for="inputCodigo" class="form-label">Código</label>
+    <input type="text" class="form-control" id="inputCodigo" disabled>
+  </div>
+
+  <div class="col-md-11">
+    <label for="inputCodigo" class="form-label">Descrição do Produto</label>
+    <input type="text" class="form-control" id="inputCodigo">
+  </div>
+
+
+
+</form>
+
+
+
+
+<br>
+</div>
