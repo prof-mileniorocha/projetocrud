6cfe5494cf77d914daa25bc862755a59a24f38ec
prof-mileniorocha <127246074+prof-mileniorocha@users.noreply.github.com>
2023-06-20 21:06:37 -0300

    Update Produto.php
    Criando métodos get e set de Produto

--- projetoaula_mvc/App/Models/Entidades/Produto.php
+++ projetoaula_mvc/App/Models/Entidades/Produto.php
@@ -1,18 +1,106 @@
 <?php 
+
 namespace App\Models\Entidades;
 
 class Produto{
+     private $id;
+     private $descricao;
+     private $marca;
+     private $modelo;
+     private $cor;
+     private $voltagem;
+     private $peso;
+     private $altura;
+     private $largura;
+     private $profundidade;
+     private $preco;
 
-    private $id;
+     public function getI(){
+          return $this->id;
+     }
 
-    public function getId(){
-        return $this->id;
-   }
+     public function setId($id){
+          $this->id = $id;
+     }
 
-   public function setId($id){
-        $this->id = $id;
-   }
-    
+     public function getDescricao(){
+          return $this->descricao;
+     }
+
+     public function setDescricao($descricao){
+          $this->descricao = $descricao;
+     }
+
+     public function getMarca(){
+          return $this->marca;
+     }
+
+     public function setMarca($marca){
+          $this->marca = $marca;
+     }
+
+     public function getModelo(){
+          return $this->modelo;
+     }
+
+     public function setModelo($modelo){
+          $this->modelo = $modelo;
+     }
+
+     public function getCor(){
+          return $this->cor;
+     }
+
+     public function setCor($cor){
+          $this->cor = $cor;
+     }
+
+     public function getVoltagem(){
+          return $this->voltagem;
+     }
+
+     public function setVoltagem($voltagem){
+          $this->voltagem = $voltagem;
+     }
+
+     public function getPeso(){
+          return $this->peso;
+     }
+
+     public function setPeso($peso){
+          $this->peso = $peso;
+     }
+
+     public function getAltura(){
+          return $this->altura;
+     }
+
+     public function setAltura($altura){
+          $this->altura = $altura;
+     }
+
+     public function getLargura(){
+          return $this->largura;
+     }
+
+     public function setLargura($largura){
+          $this->largura = $largura;
+     }
+
+     public function getProfundidade(){
+          return $this->profundidade;
+     }
+
+     public function setProfundidade($profundidade){
+          $this->profundidade = $profundidade;
+     }
+
+     public function getPreco(){
+          return $this->preco;
+     }
+
+     public function setPreco($preco){
+          $this->preco = $preco;
+     }
 }
 ?>
-
